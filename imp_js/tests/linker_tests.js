6d38ad2dbb6f426136adc714e5818afc3d051a05
BaffledBanana <rolands.laucis@gmail.com>
2022-02-05 17:54:55 +0200

    refactor and nested code block tests

--- imp_js/tests/linker_tests.js
+++ imp_js/tests/linker_tests.js
@@ -1,5 +1,5 @@
 //node grammer_tests.js
-import { LinkPat, pat_grams as p, CastTagToRegex, CastPatToRegex, ResolveFromContext } from "../linker.js";
+import { LinkPat, pat_grams as p, CastPatToRegex, ResolveFromContext, LinkTar, tar_grams as t } from "../linker.js";
 
 Array.prototype.equals = function (arr) {
     if (!arr)
@@ -21,7 +21,20 @@ function test(name, generated, expected) {
     if (generated.equals(expected))
         console.log(`✔️\t${name} PASS`)
     else
-        console.log(`❌\t${name} FAIL - GOT [${[generated[0].name, ...generated.slice(1)]}] BUT EXPECTED [${[expected[0].name, ...expected.slice(1)]}]`)
+        console.log(`❌\t${name} FAIL - GOT\n${[generated[0].name, ...generated.slice(1)]}\nBUT EXPECTED\n${[expected[0].name, ...expected.slice(1)]}\n`)
+}
+
+/**
+ * Testing function
+ * @param {string} name
+ * @param {Array} generated
+ * @param {Array} expected
+ */
+function jtest(name, generated, expected) {
+    if (JSON.stringify(generated) == JSON.stringify(expected))
+        console.log(`✔️\t${name} PASS`)
+    else
+        console.log(`❌\t${name} FAIL - GOT\n${JSON.stringify(generated)}\nBUT EXPECTED\n${JSON.stringify(expected)}\n`)
 }
 
 /**
@@ -34,43 +47,57 @@ function test_singles(name, generated, expected) {
     if (generated === expected)
         console.log(`✔️\t${name} PASS`)
     else
-        console.log(`❌\t${name} FAIL - GOT [${generated}] BUT EXPECTED [${expected}]`)
+        console.log(`❌\t${name} FAIL - GOT\n${generated}\nBUT EXPECTED\n${expected}\n`)
 }
 
-// process.exit(0)
+const test_cases = ['if'] //'symbol', 'variable', 'ending', 'pre_tags', 'recursive', 'if'
 
-//sym
-console.log('📝 Testing symbol tags...')
-test('symbol full tag', LinkPat('sym "smth"'), [p['sym'], 'smth'])
-test('symbol short tag', LinkPat('"smth"'), [p[''], 'smth'])
+if (test_cases.includes('symbol')) {
+    console.log('📝 Testing symbol tags...')
+    test('symbol full tag', LinkPat('sym "smth"'), [p['sym'], 'smth'])
+    test('symbol short tag', LinkPat('"smth"'), [p[''], 'smth'])
+}
 
-//var
-console.log('📝 Testing variable tags...')
-test('variable full tag', LinkPat('var "x"'), [p['var'], 'x'])
-test('variable tag no label', LinkPat('var'), [p['var']])
+if (test_cases.includes('variable')) {
+    console.log('📝 Testing variable tags...')
+    test('variable full tag', LinkPat('var "x"'), [p['var'], 'x'])
+    test('variable tag no label', LinkPat('var'), [p['var']])
 
-var match = "let x y".match(CastPatToRegex(['"let "', 'var "x"', '" "', 'var'].map(tag => LinkPat(tag))))
-test_singles('variable tag resolve to literal', ResolveFromContext(match, 1), 1)
-test_singles('variable tag resolve to literal', ResolveFromContext(match, '1'), 1)
-test_singles('variable tag resolve by index', ResolveFromContext(match, '[1]'), 'x')
-test_singles('variable tag resolve by index 2nd var', ResolveFromContext(match, '[2]'), 'y')
-test_singles('variable tag resolve by label', ResolveFromContext(match, 'x'), 'x')
+    console.log('📝 Testing variable resolving from context...')
+    var match = "let x y".match(CastPatToRegex(['"let "', 'var "x"', '" "', 'var'].map(tag => LinkPat(tag))))
+    test_singles('variable tag resolve to literal', ResolveFromContext(match, 1), 1)
+    test_singles('variable tag resolve to literal', ResolveFromContext(match, '1'), 1)
+    test_singles('variable tag resolve by index', ResolveFromContext(match, '[1]'), 'x')
+    test_singles('variable tag resolve by index 2nd var', ResolveFromContext(match, '[2]'), 'y')
+    test_singles('variable tag resolve by label', ResolveFromContext(match, 'x'), 'x')
+}
 
-//end
-console.log('📝 Testing ending tags...')
-test('end full tag', LinkPat('end "smth"'), [p['end'], 'smth'])
-test('end tag no label', LinkPat('end'), [p['end']])
+if (test_cases.includes('ending')) {
+    console.log('📝 Testing ending tags...')
+    test('end full tag', LinkPat('end "smth"'), [p['end'], 'smth'])
+    test('end tag no label', LinkPat('end'), [p['end']])
+}
 
-//pretags
-console.log('📝 Testing pre-tags...')
-test('new line capture tag', LinkPat('n'), [p['n']])
-test('indent capture tag', LinkPat('i'), [p['i']])
+if (test_cases.includes('pre_tags')) {
+    console.log('📝 Testing pre-tags...')
+    test('new line capture tag', LinkPat('n'), [p['n']])
+    test('indent capture tag', LinkPat('i'), [p['i']])
+}
 
-//recursive
-console.log('📝 Testing recursive tags...')
-test('rec start tag', LinkPat('rec'), [p['rec']])
-test('rec end tag', LinkPat('/rec'), [p['/rec']])
+if (test_cases.includes('recursive')){
+    console.log('📝 Testing recursive tags...')
+    test('rec start tag', LinkPat('rec'), [p['rec']])
+    test('rec end tag', LinkPat('/rec'), [p['/rec']])
+}
 
-//add
-// console.log('📝 Testing operator tags...')
-// test_singles('add tag literals', ResolveFromContext(match, 'x'), 'x')
+if (test_cases.includes('if')) {
+    console.log('📝 Testing IF tags...')
+    // console.log(JSON.stringify())
+
+    jtest('single IF block', LinkTar(['before block', '1[if [1]>2]', 'inside', '1[/if]', 'after block']), ['before block', [t['if'],'[1]>2', ['inside']], 'after block'])
+    jtest('single nested IF block', LinkTar(['1[if [1]>2]', ' var ', '2[if [1]>2]', ' something ', '2[/if]', '1[/if]']), [[t['if'], "[1]>2", [" var ", [t['if'], "[1]>2", [" something "]]]]])
+
+    jtest('double IF block', LinkTar(['1[if [1]>2]', ' var ', '1[/if]', '2[if [1]>2]', ' something ', '2[/if]']), [[t['if'], "[1]>2", [" var "]], [t['if'],"[1]>2",[" something "]]])
+    jtest('double nested IF block', LinkTar(['1[if [1]>2]', ' var ', '2[if [1]>2]', ' something ', '2[/if]', '3[if [1]>2]', ' something else ', '3[/if]', '1[/if]']), [[t['if'], "[1]>2", [" var ", [t['if'], "[1]>2", [" something "]], [t['if'],"[1]>2",[" something else "]]]]])
+}
+
