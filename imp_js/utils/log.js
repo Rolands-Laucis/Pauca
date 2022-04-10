44328775fa0bf105702c9d54a8db5f2c129cdd0b
BaffledBanana <rolands.laucis@gmail.com>
2022-04-10 17:22:49 +0300

    console colorful logging

--- imp_js/utils/log.js
+++ imp_js/utils/log.js
@@ -1,5 +1,34 @@
 let last_time = 0
 
+//https://stackoverflow.com/questions/9781218/how-to-change-node-jss-console-font-color
+const console_colors = {
+    Reset:"\x1b[0m",
+    Bright:"\x1b[1m",
+    Dim:"\x1b[2m",
+    Underscore:"\x1b[4m",
+    Blink:"\x1b[5m",
+    Reverse:"\x1b[7m",
+    Hidden:"\x1b[8m",
+
+    FgBlack:"\x1b[30m",
+    FgRed:"\x1b[31m",
+    FgGreen:"\x1b[32m",
+    FgYellow:"\x1b[33m",
+    FgBlue:"\x1b[34m",
+    FgMagenta:"\x1b[35m",
+    FgCyan:"\x1b[36m",
+    FgWhite:"\x1b[37m",
+
+    BgBlack:"\x1b[40m",
+    BgRed:"\x1b[41m",
+    BgGreen:"\x1b[42m",
+    BgYellow:"\x1b[43m",
+    BgBlue:"\x1b[44m",
+    BgMagenta:"\x1b[45m",
+    BgCyan:"\x1b[46m",
+    BgWhite:"\x1b[47m"
+}
+
 /**
  * @param {object[]} objs
  */
@@ -14,7 +43,7 @@ export function log(...objs){
  * @param {boolean} exit
  */
 export function error(msg, ...objs) {
-    console.log(`${prefix('MARBLE ERROR')} 😐 Bruh...\n`, msg)
+    console.log(`${prefix('MARBLE ERROR', console_colors.FgRed)} 😐 Bruh...\n`, msg)
     if(objs)
         console.log(...objs)
     process.exit(1)
@@ -26,7 +55,7 @@ export function error(msg, ...objs) {
  * @param {boolean} exit
  */
 export function TODO(msg = '', obj = null, exit=false){
-    console.log(`${prefix('MARBLE TODO')} Not implemented:`, msg)
+    console.log(`${prefix('MARBLE TODO', console_colors.FgYellow)} Not implemented:`, msg, '\n')
     if (obj)
         console.log(obj)
     if (exit)
@@ -48,6 +77,6 @@ export function endTimer(reset = false){
     return ((ms[0] * 1000) + (ms[1] / 1000000)).toFixed(2)
 }
 
-function prefix(msg = 'MARBLE'){
-    return `[${msg}]`
+function prefix(msg = 'MARBLE', color=console_colors.Reset){
+    return `${color}[${msg}]${console_colors.Reset}`
 }
