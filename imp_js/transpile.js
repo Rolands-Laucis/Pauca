6de5e727f507ca22972c0b108da6646488338866
BaffledBanana <rolands.laucis@gmail.com>
2022-04-13 21:46:16 +0300

    comments

--- imp_js/transpile.js
+++ imp_js/transpile.js
@@ -5,6 +5,9 @@ import { ResolvePatternsToRegex, ResolveTarget } from './resolver.js'
 import { info, startTimer, error, log } from "./utils/log.js"
 import sizeof from 'object-sizeof'
 
+// import { ExportParsed } from './utils/fs_utils.js'
+
+//enum class for marble transpilation modes
 export class TranspileMode {
     static SINGLE = new TranspileMode('SINGLE')//transpile as single match and single generated output
     static MULTIPLE = new TranspileMode('MULTIPLE') //transpile each pattern for every match in the input sequentially generating its output
@@ -13,7 +16,7 @@ export class TranspileMode {
     constructor(n) {this.name = n}
 }
 
-
+//https://stackoverflow.com/questions/20817618/is-there-a-splice-method-for-strings
 String.prototype.splice = function (index, count, add='') { return this.slice(0, index) + add + this.slice(index + count); }
 
 /**
@@ -37,14 +40,16 @@ export function MarbleTranspile(syntax, source, { mode = TranspileMode.REPLACE, 
 
     //parse the marble syntax into a ready-to-use data structure
     let parse_tree = Parse(syntax)
-    // log(parse_tree)
+    // ExportParsed(parse_tree, {path:'./gen/parse_tree.json'})
+    // process.exit(1)
     if (verbose) info(`Parsed marble file. Size of parse tree object: [${sizeof(parse_tree)}] Bytes`)
     if (only_parse) error('Purposeful program termination to print parse tree:',parse_tree)
 
     //resolve the pattern tokens to a regex object
     parse_tree = ResolvePatternsToRegex(parse_tree)
     if (verbose) info(`Resolved patterns to regex`)
-    // log(parse_tree)
+    // ExportParsed(parse_tree, {path:'./gen/parse_tree.json'})
+    // process.exit(1)
     if (only_resolve_pat) error('Purposeful program termination to print parse tree after pattern resolve:', parse_tree)
 
     let output = ''
@@ -71,6 +76,7 @@ export function MarbleTranspile(syntax, source, { mode = TranspileMode.REPLACE, 
             })
             break;
         case TranspileMode.REPLACE:
+            output = source
             parse_tree.forEach((pair, i) => {
                 let offset = 0 //the match all is an iterator, which should mean that it only does the match when the next iteration is called, 
                 //but actually idfk what it does, bcs if i change the input string with the custom splice, then the match indexes are still the same as they were. Maybe since strings are immutable.
@@ -78,12 +84,11 @@ export function MarbleTranspile(syntax, source, { mode = TranspileMode.REPLACE, 
 
                 for (const m of source.matchAll(pair.pat)) { //have to loop like this, bcs matchAll returns an iterator, which is useful for large input texts.
                     const resolve = ResolveTarget(pair.tar, m.groups || {})
-                    source = source.splice(m.index + offset, m[0].length, resolve)
+                    output = output.splice(m.index + offset, m[0].length, resolve)
                     offset += resolve.length - m[0].length
                     if (verbose) info(`Resolved pattern [${i}] target to output string`)
                 }
             })
-            output = source
             break;
         // case TranspileMode. ;break;
         default: error('Usupported transpilation mode!', mode)
